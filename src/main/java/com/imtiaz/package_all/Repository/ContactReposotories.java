6d809ca428470333a782b37e1ff78532a7976aa1
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-06-11 23:27:51 +0600

    Repository working..

--- src/main/java/com/imtiaz/package_all/Repository/ContactReposotories.java
+++ src/main/java/com/imtiaz/package_all/Repository/ContactReposotories.java
@@ -1,5 +1,8 @@
 package com.imtiaz.package_all.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.imtiaz.package_all.EntityModel.ContactEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,12 +10,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ContactReposotories extends JpaRepository<ContactEntity, Long> {
 
-    // pagination method
-    @Query("from ContactEntity as c where c.user.id =:userId")
-    //currentPage-page
-    //Contact Per page - 5
-    public Page<ContactEntity> findContactsByUser(@Param("userId") Long userId, Pageable pePageable);
+public interface ContactReposotories extends JpaRepository<ContactEntity, Long>{
+
+    // for pagination we use JPA repository.
+    @Query("from ContactEntity as c where c.userEntity.id =:id")
+    public Page<ContactEntity> findContactByUser(@Param("id") Long userID,Pageable pageable);
+
+    @Query("from ContactEntity as c where c.cid =:id")
+    public Optional<ContactEntity> findById(Long id);
 
 }
--- src/main/java/com/imtiaz/package_all/Repository/UserRepositories.java
+++ src/main/java/com/imtiaz/package_all/Repository/UserRepositories.java
@@ -1,11 +1,26 @@
 package com.imtiaz.package_all.Repository;
+import javax.transaction.Transactional;
 
 import com.imtiaz.package_all.EntityModel.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRepositories extends JpaRepository<UserEntity, Long> {
-    @Query("select u from UserEntity u where u.email = :email")
+
+
+    @Query("select u from UserEntity u where u.email =:email")
     public UserEntity getUserByUserName(@Param("email") String email);
+
+    @Modifying
+    @Transactional
+    @Query("delete from UserEntity u where u.email =:email")
+    public Integer deleteUserByUserName(@Param("email") String email);
+
+
 }
+
