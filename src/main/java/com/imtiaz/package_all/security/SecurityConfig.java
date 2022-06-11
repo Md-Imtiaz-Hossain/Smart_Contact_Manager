33e6d95ce5e1b7ed88488d244543c1907eb450de
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-06-11 23:15:42 +0600

    SecurityConfig java file checked.

---
+++ src/main/java/com/imtiaz/package_all/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.imtiaz.package_all.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER").antMatchers("/")
+                .permitAll().and().formLogin().loginPage("/signin").
+                loginProcessingUrl("/doLogin").
+                defaultSuccessUrl("/user/index").failureUrl("/login-fail").
+                and().csrf().disable();
+    }
+
+    @Bean
+    public UserDetailsService getUserDetailServiceImpl() {
+        return new UserDetailsServiceImpl();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(getUserDetailServiceImpl());
+        daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+
+}
--- src/main/java/com/imtiaz/package_all/security/UserDetailsServiceImpl.java
+++ src/main/java/com/imtiaz/package_all/security/UserDetailsServiceImpl.java
@@ -8,16 +8,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class CustomUserDetailsService implements UserDetailsService {
-
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-
         User userEntity = userRepo.getUserByUserName(username);
         if(userEntity == null) {
             throw new UsernameNotFoundException("Username Not Found !!! ");
